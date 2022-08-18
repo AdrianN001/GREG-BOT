cb8665165a591276a93b7bd8d7c69e775db9e9d3
adrian <gogosadrian@outlook.com>
2022-08-18 22:25:44 +0200

    debug

--- commands/rps.ts
+++ commands/rps.ts
@@ -25,9 +25,9 @@ export default {
         const reply_collector = new MessageCollector(message.channel,{time: 30000000, max: 1})
         reply_collector.filter = (reply: DISCORDjs.Message) => reply.author.id === player_2.id
 
-        let is_playing = false;
         
-        reply_collector.on("collect", async (message: DISCORDjs.Message) => {
+        
+        reply_collector.on("collect", (message: DISCORDjs.Message) => {
             if (message.content.toLowerCase() !== "igen" ) {
                 message.channel.send("Visszautasitva ):"); 
                 reply_collector.stop(); 
@@ -36,59 +36,55 @@ export default {
             else { 
                 message.channel.send("Elfogadva");
                 reply_collector.stop();
-                is_playing = true; 
                 
-            }
-        }) 
-        if (!is_playing ){return;}
+                console.log(player_1.username)
+                console.log(player_2.username)
 
-        console.log(player_1.username)
-        console.log(player_2.username)
+                let embeds: DISCORDjs.Message[] = []; 
 
-        let embeds: DISCORDjs.Message[] = []; 
+                [player_1, player_2].forEach(async (player: DISCORDjs.User) => 
+                {
+                    
+                    embeds.push(await player.send({embeds: [game.generate_main_embed(player == player_1 ? player_2: player_1)] , components: [RPS.generate_rows()]}))
+                }) // send game for every player
 
-        [player_1, player_2].forEach(async (player: DISCORDjs.User) => 
-        {
+
+
+                //GATHERING INFO
+                let choices : Map <DISCORDjs.User, string> = new Map<DISCORDjs.User, string>([
+                    [player_1, "semmi"],
+                    [player_2, "asd"]
+                ])
+
+                const filter = (interaction: any) => interaction.user.id === player_1.id && interaction.user.id === player_2.id
+                
+
+                // attach collector to every embed that has been sended
+
+                embeds.forEach(message => 
+                {
+                    const collector = message.createMessageComponentCollector({filter:filter,
+                    componentType: "SELECT_MENU",
+                    time: 1000})
+
+                    collector.on("collect", async (collected) => {
+                        if ([...choices.keys()].includes(collected.user)){
             
-            embeds.push(await player.send({embeds: [game.generate_main_embed(player == player_1 ? player_2: player_1)] , components: [RPS.generate_rows()]}))
-        }) // send game for every player
+                            const value = collected.values[0]
+                            choices.set(collected.user, value)
+                        }
+                    })
+                })
+                
 
-
-
-        //GATHERING INFO
-        let choices : Map <DISCORDjs.User, string> = new Map<DISCORDjs.User, string>([
-            [player_1, "semmi"],
-            [player_2, "asd"]
-        ])
-
-        const filter = (interaction: any) => interaction.user.id === player_1.id && interaction.user.id === player_2.id
-        
-
-        // attach collector to every embed that has been sended
-
-        embeds.forEach(message => 
-        {
-            const collector = message.createMessageComponentCollector({filter:filter,
-            componentType: "SELECT_MENU",
-            time: 1000})
-
-            collector.on("collect", async (collected) => {
-                if ([...choices.keys()].includes(collected.user)){
-    
-                    const value = collected.values[0]
-                    choices.set(collected.user, value)
+                for (const [key, value] of choices)
+                {
+                    console.log(`${key} -> ${value}`)
                 }
-            })
-        })
-        
-
-        for (const [key, value] of choices)
-        {
-            console.log(`${key} -> ${value}`)
+            
+                
         }
-       
-        
-
+    })
     }
 
 
