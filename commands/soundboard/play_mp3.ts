264032a0c96d1fce6f3517187518b90111f98b32
adrian <gogosadrian@outlook.com>
2022-12-17 07:38:32 -0500

    play_mp3 added

---
+++ commands/soundboard/play_mp3.ts
@@ -0,0 +1,40 @@
+import { ICommand } from 'wokcommands';
+// @ts-ignroe
+import { joinVoiceChannel, createAudioResource, createAudioPlayer } from "@discordjs/voice"
+
+
+
+
+
+export default {
+    category: 'soundboard',
+    name: 'play',
+    description: 'Egy adott file-t lejatszik',
+
+
+
+    callback: ({ message, args }) => {
+        if (!message.guild?.id) {
+            message.reply("HIBA");
+            return;
+        }
+        const connection = joinVoiceChannel({
+            channelId: message.channel.id,
+            guildId: message.guild!.id,
+            adapterCreator: message.guild!.voiceAdapterCreator,
+        });
+
+        const file: string | undefined = message.attachments.first()?.url;
+        if (!file) { message.reply("Nem lett hang fajl hozzacsatolva az üzenethez"); return; }
+        const resource =
+            createAudioResource(file!, {
+                inlineVolume: true
+            })
+
+        const player = createAudioPlayer();
+        connection.subscribe(player)
+        player.play(resource)
+    }
+
+
+} as ICommand;
